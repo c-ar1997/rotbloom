d8290add91a8fed1d634d8a8350ee0db526701f9
c_ar <kanaroakadar@gmail.com>
2025-12-31 14:01:32 +0100

    guh

---
+++ src/main/java/net/car/rotbloom/RotbloomClient.java
@@ -0,0 +1,11 @@
+package net.car.rotbloom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RotbloomClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/car/rotbloom/item/ModItem.java
@@ -0,0 +1,4 @@
+package net.car.rotbloom.item;
+
+public class ModItem  {
+}
